747445f219843a19eac7423ac3d88bccb2d283b5
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-02-06 15:10:33 -0500

    Update views.py

--- copyai/text/views.py
+++ copyai/text/views.py
@@ -2,11 +2,10 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from youtube_transcript_api import YouTubeTranscriptApi
 
-@api_view(['GET'])
+@api_view(['POST'])
 def orchestrator(request):
-    url = 'https://www.youtube.com/watch?v=Kjt_EDgFlkk'
-    print("running---------------")
-    #url = request.data['link']
+    #url = 'https://www.youtube.com/watch?v=Kjt_EDgFlkk'
+    url = request.data['link']
     code_id = url.rsplit("=")[-1]
 
     d = YouTubeTranscriptApi.get_transcript(code_id)
