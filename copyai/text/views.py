576e466e9af6d8686c592bc3cdf41ae8fad8883b
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-02-06 14:18:43 -0500

    sanaz

--- copyai/text/views.py
+++ copyai/text/views.py
@@ -4,8 +4,8 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 @api_view(['POST'])
 def orchestrator(request):
-
-    url = request.data['link']
+    url = 'https://www.youtube.com/watch?v=Kjt_EDgFlkk'
+    #url = request.data['link']
     code_id = url.rsplit("=")[-1]
 
     d = YouTubeTranscriptApi.get_transcript(code_id)
