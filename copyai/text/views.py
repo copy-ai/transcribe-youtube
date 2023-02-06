dea0fb4745434a423794ea0ecad4baa29acc7d9d
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-02-06 15:08:28 -0500

    Update views.py

--- copyai/text/views.py
+++ copyai/text/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from youtube_transcript_api import YouTubeTranscriptApi
 
-@api_view(['POST'])
+@api_view(['GET'])
 def orchestrator(request):
     url = 'https://www.youtube.com/watch?v=Kjt_EDgFlkk'
     print("running---------------")
